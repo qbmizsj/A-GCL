077900ed5f69aaf61f0f39446253f9600b4c1574
sjzhang <108256124+qbmizsj@users.noreply.github.com>
2023-05-14 13:54:47 +0000

    Delete A-GCL directory

--- A-GCL/unsupervised/encoder/__init__.py
+++
@@ -1,3 +0,0 @@
-from .tu_encoder import TUEncoder
-from .gatv2_encoder import GATv2Encoder
-from .uni_encoder import UniEncoder
--- A-GCL/unsupervised/learning/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ginfomax import InfoGraph
-from .ginfominmax import GInfoMinMax
-from .gsimclr import GSimCLR
-from .classifier import Classifier
